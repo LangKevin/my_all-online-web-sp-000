747e250352062ba529fa1a4aba827f66ba288912
kevin lang <langkevina@gmail.com>
2019-05-25 16:46:33 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -15,4 +15,14 @@ def my_all?(collection)
   end
 end
 
-block_return_values = [true, false, false]
+# block_return_values = [true, false, false]
+# my_all?([1,2,3]) {|1| 1 < 2}
+#   #=> true
+#   def my_all?(collection)
+#     i = 0
+#     block_return_values = []
+#     while i < collection.length
+#       block_return_values << yield(collection[i])
+#       i = i + 1
+#     end
+#   end
